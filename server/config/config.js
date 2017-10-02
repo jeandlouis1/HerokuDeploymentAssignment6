a03289f9a7a43252dd98e9c1f21d6b354194d6b9
jLouis <jeandlouis1@gmail.com>
2017-10-02 10:45:39 -0400

    test

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://jean:louis@ds129344.mlab.com:29344/ass3', //place the URI of your mongo database here.
+  }, 
+  googleMaps: {
+    key: 'AIzaSyCUm93K_0r4V8XvnN1uHg5v0mAUdA3s0aI'
+  },
+  port: 8080
+};
